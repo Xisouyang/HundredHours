4d3799ba2c518e4a9bb0bb1d17dfbb74addbc4b7
Xi Ouyang <xisouyang@gmail.com>
2019-09-04 14:11:48 -0700

    [add] first commit

---
+++ 100HoursApp/100HoursDev/HomeViewController.swift
@@ -0,0 +1,38 @@
+//
+//  ViewController.swift
+//  100HoursDev
+//
+//  Created by Stephen Ouyang on 9/2/19.
+//  Copyright © 2019 Stephen Ouyang. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+    
+    static var goalsArr: [String] = []
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+        navigationItem.title = "Goals"
+        
+        if HomeViewController.goalsArr.isEmpty {
+            useEmptyStateView()
+        }
+    }
+    
+    func useEmptyStateView() {
+        let emptyView = NoGoalsView(frame: view.frame)
+        view.addSubview(emptyView)
+        emptyView.newGoalButton.addTarget(self, action: #selector(newGoalTapped), for: .touchUpInside)
+    }
+}
+
+extension HomeViewController {
+    
+    @objc func newGoalTapped() {
+        print("tapped")
+    }
+}
+
