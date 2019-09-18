f68096f31a4c190c47d65c1c4b27cb0fba13c82e
Xi Ouyang <xisouyang@gmail.com>
2019-09-17 23:31:14 -0700

    [add] added time stamp title

--- HundredHoursApp/HundredHoursDev/Views/TimeStampView.swift
+++ HundredHoursApp/HundredHoursDev/Views/TimeStampView.swift
@@ -10,6 +10,15 @@ import UIKit
 
 class TimeStampsView: UIView {
     
+    let timeStampTitle: UILabel = {
+        let label = UILabel()
+        label.text = "Time Stamps"
+        label.textAlignment = .left
+        label.textColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
+        label.font = UIFont(name: "Avenir-Heavy", size: 25)
+        return label
+    }()
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
         backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
@@ -18,9 +27,20 @@ class TimeStampsView: UIView {
         self.layer.borderWidth = 0.5
         self.layer.borderColor = #colorLiteral(red: 0.6000000238, green: 0.6000000238, blue: 0.6000000238, alpha: 1)
         
+        addSubview(timeStampTitle)
+        titleConstraints()
     }
     
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+    func titleConstraints() {
+        
+        timeStampTitle.translatesAutoresizingMaskIntoConstraints = false
+        timeStampTitle.widthAnchor.constraint(equalTo: self.widthAnchor, multiplier: 0.6).isActive = true
+        timeStampTitle.heightAnchor.constraint(equalToConstant: 50).isActive = true
+        timeStampTitle.topAnchor.constraint(equalToSystemSpacingBelow: self.topAnchor, multiplier: 2).isActive = true
+        timeStampTitle.leftAnchor.constraint(equalToSystemSpacingAfter: self.leftAnchor, multiplier: 2).isActive = true
     }
 }
