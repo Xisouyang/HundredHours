aa9fa0ff15a6ea2c3f0903d3c3a5cdaf0225c4c6
Stephen Ouyang <xisouyang@gmail.com>
2020-04-13 07:28:08 -0700

    [update] changed status bar color to white

--- HundredHoursApp/HundredHoursDev/AppDelegate.swift
+++ HundredHoursApp/HundredHoursDev/AppDelegate.swift
@@ -58,7 +58,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
-        
         let nav = UINavigationController()
         configNavigation(nav)
         coordinator = MainCoordinator(navigationController: nav)
