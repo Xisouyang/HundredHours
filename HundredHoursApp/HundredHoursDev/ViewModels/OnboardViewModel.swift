17119b380eecda77ef2ec96e2ccb92ef1784be20
Stephen Ouyang <xisouyang@gmail.com>
2020-04-14 13:52:12 -0700

    [update] changed app icon, changed onboarding message

--- HundredHoursApp/HundredHoursDev/ViewModels/OnboardViewModel.swift
+++ HundredHoursApp/HundredHoursDev/ViewModels/OnboardViewModel.swift
@@ -12,7 +12,7 @@ class OnboardViewModel {
     
     let dataSource: [OnboardItem] = [
         OnboardItem(title: "Create Goal", description: "Create a goal or activity to keep track of", imgName: "createIconSVG"),
-        OnboardItem(title: "Start Timer", description: "Start timer when you're ready to work on your goal or activity", imgName: "timerIconSVG"),
+        OnboardItem(title: "Start Timer", description: "Start timer by tapping the goal when you're ready to work", imgName: "timerIconSVG"),
         OnboardItem(title: "Track Progress", description: "Visually track your progress for increased motivation", imgName: "progressIconSVG"),
         OnboardItem(title: "Turn on Notifications", description: "Receive reminders to reach your goals", imgName: "notificationsIcon")
     ]
